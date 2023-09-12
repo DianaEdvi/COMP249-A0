cd499a15378290e9ad2f1b6cb0b0e494b3a6ee16
Diana <115322193+VinVoide@users.noreply.github.com>
2023-09-12 00:18:30 -0400

    Update A0_Driver.java
    added stuff

--- A0_Driver.java
+++ A0_Driver.java
@@ -14,13 +14,15 @@ public class A0_Driver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner keyboard = new Scanner(System.in); //instantiate
 		
-		System.out.println("Welcome to the bookstore inventory system. "
+		System.out.println("Welcome to the bookstore inventory system.\n\n"
 				+ "Please enter the maximum number of books your bookstore can contain: ");
 		
 		int maxBooks = verifyInt(keyboard);
+		
+		Book.setMaxNumBooks(maxBooks);
+		
 		Book[] inventory = new Book[maxBooks];
 		
 		mainMenu();
@@ -30,101 +32,137 @@ public class A0_Driver {
 		int counter02 = 0;
 		
 		while (!(input == 5)) {
-			System.out.println("Please enter your choice: ");
+			System.out.println("Please enter your choice from 1 to 5: ");
 			input = verifyInput(keyboard);
+			//Option 1
 			if (input == 1) {	
 				counter = verifyPassword(keyboard, counter);
 				System.out.println("How many books would you like to enter?");
 				int numBooks = verifyInt(keyboard);
+				int totalBooks = 0;
 				
-				for (int i = 0; i < numBooks; i++) {
-					inventory[i] = new Book(); 
-				}
-				System.out.println(Book.getNumBooks());
-				
-				int emptySlots = inventory.length - Book.getNumBooks();
-				
-				if (emptySlots <= 0) {
-					System.out.println("There is no more inventory space for new books"); //would you like to remove one?
-				}
-				
-				else if (emptySlots < numBooks){
-					System.out.println("There are only " + emptySlots + " books left in your inventory."
-							+ " Would you like to add" +  emptySlots + " books?\n"
-									+ "   1.   Yes\n   2.   No");
-					int s = verifyInt(keyboard);
+				//Sort new books into inventory array
+				if ((Book.getMaxNumBooks()- Book.getTotalNumBooks() >= numBooks )){
+					totalBooks = Book.getTotalNumBooks();
 					
-					//create new books 
-					if (s == 1) {
-						for (int i = 0; i < emptySlots; i++) {
-							for (int j = 0; j < 3; j++) {
-								String[] attributes = new String[4];
-								
-								attributes[0] = "Title";
-								attributes[1] = "Author";
-								attributes[2] = "ISBN";
-								attributes[3] = "Price";
-								System.out.println("Please enter the " + attributes[j]);
-								
-							}
-							inventory[i + Book.getNumBooks()] = new Book();
-						}
-							
+					for (int i = 0; i < numBooks; i++) {
+						inventory[i + totalBooks] = new Book(); //parameters?
 					}
 					
+					System.out.println("Books have been added successfully");
+				}
+				//Checks the amount of space left in the inventory and prompts user accordingly  
+				else if (Book.getMaxNumBooks() - Book.getTotalNumBooks() < numBooks) {
+					//No more inventory space
+					if (Book.getMaxNumBooks() - Book.getTotalNumBooks() == 0) {
+						System.out.println("There is no more inventory space for new books"); //would you like to remove one?
+					}
+					//Informs user of remaining space and prompts user for input
 					else {
-						System.out.println("Returning to the main menu...");
+						System.out.println("There are only " + (Book.getMaxNumBooks() - Book.getTotalNumBooks())
+								+ " spots left in your inventory. Would you like to add " + (Book.getMaxNumBooks() - Book.getTotalNumBooks()) 
+								+ " books?\n");
+						
+						System.out.println("   1.   Yes\n   2.   No");
+						int s = verifyInt(keyboard);
+						while (!(s == 2)) {
+							if (s == 1) {
+								for (int i = 0; i < (Book.getMaxNumBooks() - Book.getTotalNumBooks()); i++) {
+									inventory[i + totalBooks] = new Book(); //parameters?
+								}
+								System.out.println("Books have been added successfully");
+								break;
+							}
+							//Input validation
+							else {
+								System.out.println("Please choose one of the available options");
+								System.out.println("   1.   Yes\n   2.   No");
+								s = verifyInt(keyboard);
+							}
+						}
+						System.out.println("Returning to main menu...\n");
 						mainMenu();
 					}
-					
 				}
-				
 			}
-			
+			//Option 2
 			else if (input == 2) {
 				counter = verifyPassword(keyboard, counter02);
-				System.out.println("Which book number do you wish to update?");
-				
+				int option = 0;
 				
 				do {
-					int s = verifyInt(keyboard);
-					if (inventory[s].getISBN() == 0) {
-						System.out.println("There is no book associated with this book number. " //what if input is bigger than maxbooks?
-								+ "What would you like to do?"
-								+ "   1.   Enter new book number"
-								+ "   2.   Return to main menu");
-						
-					//this code is fucked rn
-						
-						if (verifyInt(keyboard) == 1) {
-							continue;
-						}
-						else {}
-				
-					}
+					System.out.println("Which book number do you wish to update?");//what if you choose last book? index.
+					int bookNum = verifyInt(keyboard);
 					
+					if (inventory[bookNum] == null) {
+						System.out.println("There is no book associated with this book number. " //what if input is bigger than maxbooks?
+								+ "What would you like to do?\n"
+								+ "   1.   Enter new book number\n"
+								+ "   2.   Return to main menu");
+						option = verifyInt(keyboard);
+						if (option == 2) {
+							break;
+						}
+						
+						while (!(option == 1) && !(option == 2) ) {
+							System.out.println("Please choose one of the available options");
+							option = verifyInt(keyboard);
+						}
+					}
 					else {
-						//display book attributes method
+						System.out.println("Book # " + bookNum);
+						System.out.println(inventory[bookNum].toString());
+						int choice = 0;
+						do {
+							System.out.println("\nWhat information would you like to change?\n" //still need to change the things
+									+ "   1.   author\n"
+									+ "   2.   title\n"
+									+ "   3.   ISBN\n"
+									+ "   4.   price\n"
+									+ "   5.   back to book selection\n" //there is an issue here somewhere
+									+ "Enter your choice from 1 to 5: ");
+							choice = verifyInt(keyboard);
+						}
+						while (!(choice == 5));	
 					}
 				}
-				while (verifyInt(keyboard) == 1);
 				
+				while (!(option == 2));
+				
+				System.out.println("Returning to main menu...\n");
+				mainMenu();
 				
 			}
-			
+			//Option 3
 			else if (input == 3) {
-				System.out.println("Enter an author name: ");
+				System.out.println("Enter an author name: "); //Case sensitive?
+				keyboard.nextLine();
+				String author = keyboard.nextLine();
+				Book.findBooksBy(inventory, author); //defs has an issue with null stuff
+				System.out.println("\nReturning to main menu...\n");
+				mainMenu();
 			}
-			
+			//Option 4
 			else if (input == 4) {
 				System.out.println("Enter a maximum value: ");
+				double price = 0;
+				while (!keyboard.hasNextDouble()) {
+					System.out.println("Invalid input. Please enter a number: ");
+					keyboard.next();
+				}
+				
+				price = keyboard.nextInt();
+				
+				Book.findCheaperThan(inventory, price);
+				System.out.println("Returning to main menu...\n");
+				mainMenu();
 			}
 		}
 		
 		System.out.println("Thank you for using the Bookstore Inventory Program. The program will now terminate.");
 		System.exit(0);	
 	}
-	
+	//Displays main menu
 	public static void mainMenu() {
 		System.out.println("What do you want to do?\n   "
 				+ "1.   Enter new books (password required)\n   "
@@ -133,7 +171,7 @@ public class A0_Driver {
 				+ "4.   Display all books under a certain price\n   "
 				+ "5.   Quit\n");
 	}
-	
+	//Verifies that the user's input is an integer
 	public static int verifyInt(Scanner keyboard) {
 		int input = 0;
 
@@ -146,18 +184,18 @@ public class A0_Driver {
 
 		return input;
 	}
-	
+	//Verifies that the user chooses one of the offered options
 	public static int verifyInput(Scanner keyboard) {
 		
 		int input = verifyInt(keyboard);
 		
 		if (input < 1 || input > 5) {
-			System.out.println("Invalid input. Please enter a number between 1 and 5: ");
+			System.out.println("Invalid input.");
 		}
 		
 		return input;
 	}
-	
+	//Verifies password, and closes the program if the wrong password has been entered 12 times in a row
 	public static int verifyPassword(Scanner keyboard, int counter) {
 		
 		String password = "249";
@@ -186,7 +224,6 @@ public class A0_Driver {
 				} 
 				else {
 					System.out.println("Incorrect password. Try again.");
-
 				}
 			}
 		}
